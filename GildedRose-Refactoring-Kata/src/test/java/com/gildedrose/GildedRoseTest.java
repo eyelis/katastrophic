1d931b8d58f30c822d0a8122e66763d1818ad840
sileye ba <eyelis>
2017-07-13 13:52:26 +0200

    refacto tests

--- GildedRose-Refactoring-Kata/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose-Refactoring-Kata/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,9 +1,9 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 import java.util.Arrays;
-import java.util.List;
 
 import org.junit.Test;
 
@@ -11,60 +11,78 @@ public class GildedRoseTest {
 
 	@Test
 	public void testNameShouldNotChangeWhenUpdate() {
-		List<Item> items = Arrays.asList(new Item("foo", 0, 0));
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals("foo", app.getItems().get(0).name);
+		Item foo = new Item("foo", 0, 0);
+
+		updateQuality(foo);
+
+		assertThat(foo.name, is("foo"));
 	}
 
 	@Test
 	public void testCommonsQualityAndSellInShouldDecreaseByOneWhenUpdate() {
-		List<Item> items = Arrays.asList(new Item("+5 Dexterity Vest", 10, 20),
-				new Item("Elixir of the Mongoose", 5, 7), new Item("Conjured Mana Cake", 3, 6));
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals(9, app.getItems().get(0).sellIn);
-		assertEquals(19, app.getItems().get(0).quality);
-		assertEquals(4, app.getItems().get(1).sellIn);
-		assertEquals(6, app.getItems().get(1).quality);
-		assertEquals(2, app.getItems().get(2).sellIn);
-		assertEquals(5, app.getItems().get(2).quality);
+		Item plusFiveDextryVest = new Item("+5 Dexterity Vest", 10, 20);
+		Item elixirOfTheMongoose = new Item("Elixir of the Mongoose", 5, 7);
+		Item conjuredManaCake = new Item("Conjured Mana Cake", 3, 6);
+
+		updateQuality(plusFiveDextryVest, elixirOfTheMongoose, conjuredManaCake);
+
+		assertThat(plusFiveDextryVest.sellIn, is(9));
+		assertThat(plusFiveDextryVest.quality, is(19));
+
+		assertThat(elixirOfTheMongoose.sellIn, is(4));
+		assertThat(elixirOfTheMongoose.quality, is(6));
+
+		assertThat(conjuredManaCake.sellIn, is(2));
+		assertThat(conjuredManaCake.quality, is(5));
+
 	}
 
 	@Test
 	public void testAgedBrieQualityShouldIncreaseByOneAndSellInDecreaseByOneWhenUpdate() {
-		List<Item> items = Arrays.asList(new Item("Aged Brie", 2, 0));
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals(1, app.getItems().get(0).sellIn);
-		assertEquals(1, app.getItems().get(0).quality);
+		Item agedBrie = new Item("Aged Brie", 2, 0);
+
+		updateQuality(agedBrie);
+
+		assertThat(agedBrie.sellIn, is(1));
+		assertThat(agedBrie.quality, is(1));
 	}
 
 	@Test
 	public void testSulfurasQualityAndSellInShouldNotChangeWhenUpdate() {
-		List<Item> items = Arrays.asList(new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-				new Item("Sulfuras, Hand of Ragnaros", -1, 80));
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals(0, app.getItems().get(0).sellIn);
-		assertEquals(80, app.getItems().get(0).quality);
-		assertEquals(-1, app.getItems().get(1).sellIn);
-		assertEquals(80, app.getItems().get(1).quality);
+		Item sulfurasSellin0 = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+		Item sulfurasSellinMinus1 = new Item("Sulfuras, Hand of Ragnaros", -1, 80);
+
+		updateQuality(sulfurasSellin0, sulfurasSellinMinus1);
+
+		assertThat(sulfurasSellin0.sellIn, is(0));
+		assertThat(sulfurasSellin0.quality, is(80));
+
+		assertThat(sulfurasSellinMinus1.sellIn, is(-1));
+		assertThat(sulfurasSellinMinus1.quality, is(80));
 	}
 
 	@Test
 	public void testBackstageQualityShouldIncreaseByOneAndSellInDecreaseByOneWhenUpdate() {
-		List<Item> items = Arrays.asList(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-				new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-				new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49));
-		GildedRose app = new GildedRose(items);
+		Item backstagPasses1 = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20);
+		Item backstagePasses2 = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49);
+		Item backstagePasses3 = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49);
+
+		updateQuality(backstagPasses1, backstagePasses2, backstagePasses3);
+
+		assertThat(backstagPasses1.sellIn, is(14));
+		assertThat(backstagPasses1.quality, is(21));
+
+		assertThat(backstagePasses2.sellIn, is(9));
+		assertThat(backstagePasses2.quality, is(50));
+
+		assertThat(backstagePasses3.sellIn, is(4));
+		assertThat(backstagePasses3.quality, is(50));
+	}
+
+	private void updateQuality(Item... items) {
+		GildedRose app = new GildedRose(Arrays.asList(items));
+
 		app.updateQuality();
-		assertEquals(14, app.getItems().get(0).sellIn);
-		assertEquals(21, app.getItems().get(0).quality);
-		assertEquals(9, app.getItems().get(1).sellIn);
-		assertEquals(50, app.getItems().get(1).quality);
-		assertEquals(4, app.getItems().get(2).sellIn);
-		assertEquals(50, app.getItems().get(2).quality);
 	}
 
 }
